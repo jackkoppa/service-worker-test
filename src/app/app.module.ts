2faf544fdcc57cf7559a1dfe180e39c0ae694fb6
Jack Koppa <hello@jackkoppa.com>
2018-02-10 14:42:02 -0600

    Minimal repro of routing failing For service worker Angular apps Attempting to visit 'test-route' while offline will result in 504 error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
+import { IndexRouteComponent } from './index-route.component';
+import { TestRouteComponent } from './test-route.component';
 
 import { environment } from '../environments/environment';
 
+const APP_ROUTES = [
+    { path: 'test-route', component: TestRouteComponent },
+    { path: '',   component: IndexRouteComponent },
+    { path: '**',   redirectTo: '' }
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexRouteComponent,
+    TestRouteComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(APP_ROUTES),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
